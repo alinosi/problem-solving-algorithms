6419f686decf167143519ecb59645f2b3a6c2007
alinosi <146401086+alinosi@users.noreply.github.com>
2025-04-04 20:17:32 +0700

    Add problem descriptions and metadata for Codeforces problems 4A - Watermelon and 282A - Bit++

--- 4-282A-Bitplusplus/tes4.go
+++ 4-282A-Bitplusplus/tes4.go
@@ -16,9 +16,19 @@ func main() {
 		} else if input == "--x" || input == "x--" || input == "--X" || input == "X--" {
 			x--
 		}
-		
+
 		n--
 	}
 
 	fmt.Println(x)
 }
+
+/*
+  Run at https://codeforces.com/contest/282A/status/page/1?order=asc&filter=all&locale=en
+  Problem: 282A - Bit++ (https://codeforces.com/contest/282)
+  Contest: Codeforces Round #162 (Div. 2) (https://codeforces.com/contest/282)
+  Author: Mike Mirzayanov (http://codeforces.com/profile/mir)zayanov
+  Time: 2010-11-20 18:00:00
+  Time complexity: 62ms O(n)
+  Space complexity: 100kb O(n)
+*/
