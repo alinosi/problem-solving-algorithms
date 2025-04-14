5bae5c308505f6375edcc1a46838acb60a5d732d
alinosi <146401086+alinosi@users.noreply.github.com>
2025-04-14 20:16:58 +0700

    Add initial implementation for 231A - Team problem in Go

---
+++ 3-231A-Team/tes3.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	fmt.Scan(&n)
+
+	var a []int
+	a = make([]int, n)
+	var fix int = 0
+	var temp int = 0
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Scan(&a[j])
+			if a[j] <= 1 && a[j] >= 0 {
+				temp += a[j]
+			}
+		}
+		if temp > 1 {
+			fix++
+		}
+		temp = 0
+	}
+
+	fmt.Println(fix)
+}
