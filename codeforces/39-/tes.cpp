12feb477c55036ad9da7faa3419373824d7ca69a
alinosi <mthoriqulfadli@gmail.com>
2025-09-23 14:14:42 +0700

    Add initial implementation for problem 39: count unique horseshoes

---
+++ codeforces/39-/tes.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        
+        vector<int> hourseshoes(4);
+        
+        for (int i = 0; i<4; i++) {
+            cin >> hourseshoes[i];
+        }
+        
+        sort(hourseshoes.begin(), hourseshoes.end()); 
+
+        auto last = unique(hourseshoes.begin(), hourseshoes.end());
+        hourseshoes.erase(last, hourseshoes.end());
+        
+        if  (hourseshoes.size() - 4 != 0) {
+            cout << 4 - hourseshoes.size(); 
+        } else {
+            cout << 0;
+        }
+    
+    return 0;
+}
