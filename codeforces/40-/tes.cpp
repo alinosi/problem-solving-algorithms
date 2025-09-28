2544e231e7a44a4a34458ce908b9f249bf1f86d6
alinosi <mthoriqulfadli@gmail.com>
2025-09-28 10:12:08 +0700

    Create tes.cpp

---
+++ codeforces/40-/tes.cpp
@@ -0,0 +1,25 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    string a, b;
+    vector <int> c;
+    cin >> a >> b;
+    
+    for (int i = 0; i< a.size(); ++i){
+        if (a[i] != b[i]) {
+            c.push_back(1);
+        } else {
+            c.push_back(0);
+        }
+    }
+    
+    for (int i : c){
+        cout << i;
+    }
+    
+    return 0;
+}
