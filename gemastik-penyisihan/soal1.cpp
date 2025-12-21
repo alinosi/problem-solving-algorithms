d15e52a5f809e531df69db2a5018ae25dbafb77c
alinosi <mthoriqulfadli@gmail.com>
2025-12-21 20:44:48 +0700

    Refactor logic in soal1.cpp for improved clarity and efficiency

--- gemastik-penyisihan/soal1.cpp
+++ gemastik-penyisihan/soal1.cpp
@@ -2,29 +2,49 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     long long n, a, b;
+    long long sum = 0;
+    
     cin >> n >> a >> b;
 
-    vector<long long> goods(n);
+    long long chcp = a+b;
+
+    vector<long long>goods(n);
+
     for (long long& i : goods) {
         cin >> i;
     }
 
     sort(goods.rbegin(), goods.rend());
-
-    long long sum = 0;
-    long long group_size = a + b;
-
-    for (int i = 0; i < n; i++) {
-        if (i % group_size < a) {
-            sum += goods[i];
+    
+    if ( n == a+b) {
+        for (long long i = 0; i <a ; ++i) {
+            sum+= goods.at(i);
+        }
+    } else if ( n < a + b) {
+        for (long long n : goods) {
+            sum+= n;
+        }
+    } else {
+        long long counter = 0;
+        for (long long i = 0; i < n; i++) {
+            if (n - i < chcp && counter == 0) {
+                for (long long j = i; j< n; j++) {
+                    sum += goods.at(j);
+                }
+                i = n-1;
+            } else {
+                if ( counter > 0 && counter % a == 0) {
+                    i += b - 1; // 3
+                    counter = 0;
+                } else {
+                    sum += goods.at(i);
+                    counter++;
+                }
+            }
         }
     }
 
-    cout << sum << endl;
+    cout << sum;
 
-    return 0;
 }
