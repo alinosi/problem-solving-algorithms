aae2ab3f3db47d4080b837e925926c1ef4cd4b89
alinosi <mthoriqulfadli@gmail.com>
2025-12-31 08:55:45 +0700

    intilaized soal 3cpp

---
+++ gemastik-penyisihan/soal3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int t;
+    ll n, k, total = 0;
+
+    cin >> t;    
+    vector<int> memory(t);
+
+    // output 1
+    for (int i=0; i<t; i++) {
+        cin >> n >> k;
+        total = pow(2, n);
+        if (k == total) {
+            memory[i] = log2(total);
+        } else {
+            memory[i] = floor(log2(k)) + 1;
+        }
+
+        total = 0; 
+    }
+
+
+    for(int c : memory) {
+        cout << c;
+        // cout << output2 << endl;
+    }
+
+
+}
